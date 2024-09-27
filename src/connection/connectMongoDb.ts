346f5413711993a19a1ae3074c532e160478e27d
ArpitShukla3 <arpit900shukla@gmail.com>
2024-09-28 01:44:23 +0530

    update

--- src/connection/connectMongoDb.ts
+++ src/connection/connectMongoDb.ts
@@ -4,29 +4,6 @@ dotenv.config();
 import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 import logger from "../../logger";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   }
-//   catch (e) {
-//     console.error(e);
-//   }
-// }
 async function run()
 {
     try {
@@ -42,7 +19,6 @@ async function run()
         logger.info("mongodb failed to connect due to:",error)
     }
 }
-// export default ConnectMongo;
 
 const connectDb=run;
 export default connectDb;
